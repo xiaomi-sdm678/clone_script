326e537021c427e013d218b3099945d0ee93a1f2
ANUSH MADATHUMKARA <Anush.4376@gmail.com>
2021-08-29 00:37:05 +0530

    Update Clone Script

--- clone_mojo.sh
+++ clone_mojo.sh
@@ -1,9 +1,12 @@
+#!/bin/sh
 echo "Cloning device stuffs"
 
-git clone git@github.com:xiaomi-sdm678/android_vendor_xiaomi_sm6150-common.git -b 11 vendor/xiaomi/sm6150-common
+git clone https://gitlab.com/xiaomi-sdm678/android_vendor_xiaomi_mojito.git -b 11 vendor/xiaomi/mojito
 
-git clone git@github.com:xiaomi-sdm678/android_vendor_xiaomi_mojito.git -b 11 vendor/xiaomi/mojito
+git clone https://gitlab.com/xiaomi-sdm678/android_vendor_xiaomi_sm6150-common.git -b 11-new-4 vendor/xiaomi/sm6150-common
 
 git clone git@github.com:xiaomi-sdm678/android_device_xiaomi_mojito.git -b 11 device/xiaomi/mojito
 
-git clone git@github.com:xiaomi-sdm678/android_kernel_xiaomi_mojito.git -b 11 kernel/xiaomi/mojito
+git clone git@github.com:xiaomi-sdm678/android_device_xiaomi_sm6150-common.git -b 11 device/xiaomi/sm6150-common
+
+git clone git@github.com:xiaomi-sdm678/android_kernel_xiaomi_mojito.git -b 11 kernel/xiaomi/sm6150
