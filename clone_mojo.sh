4bbe170517f1fb0ac19b661d0d6d0e6b8460692d
ANUSH MADATHUMKARA <Anush.4376@gmail.com>
2022-10-13 13:02:37 +0530

    Time for A13

--- clone_mojo.sh
+++ clone_mojo.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 echo "Cloning device stuffs"
 
-git clone https://gitlab.com/xiaomi-sdm678/android_vendor_xiaomi_mojito.git -b 12 vendor/xiaomi/mojito
+git clone https://gitlab.com/xiaomi-sdm678/android_vendor_xiaomi_mojito.git -b 13 vendor/xiaomi/mojito
 
-git clone https://gitlab.com/xiaomi-sdm678/android_vendor_xiaomi_sm6150-common.git -b 12 vendor/xiaomi/sm6150-common
+git clone https://gitlab.com/xiaomi-sdm678/android_vendor_xiaomi_sm6150-common.git -b 13 vendor/xiaomi/sm6150-common
 
-git clone git@github.com:xiaomi-sdm678/android_device_xiaomi_mojito.git -b 12 device/xiaomi/mojito
+git clone git@github.com:xiaomi-sdm678/android_device_xiaomi_mojito.git -b 13 device/xiaomi/mojito
 
-git clone git@github.com:xiaomi-sdm678/android_device_xiaomi_sm6150-common.git -b 12 device/xiaomi/sm6150-common
+git clone git@github.com:xiaomi-sdm678/android_device_xiaomi_sm6150-common.git -b 13 device/xiaomi/sm6150-common
 
-git clone git@github.com:xiaomi-sdm678/android_kernel_xiaomi_mojito.git -b 12 kernel/xiaomi/sm6150
+git clone git@github.com:xiaomi-sdm678/android_kernel_xiaomi_mojito.git -b 13 kernel/xiaomi/sm6150
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-19.1 hardware/xiaomi
+git clone git@github.com:xiaomi-sdm678/android_hardware_xiaomi.git -b 13 hardware/xiaomi
